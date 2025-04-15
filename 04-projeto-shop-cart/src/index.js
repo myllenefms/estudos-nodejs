9d3a3da615abeb57f891089ab9c99f070ce14b14
Myllene Mendes <myllenefms@hotmail.com>
2025-04-15 11:26:26 -0300

    lista de desejos com estrutura já criada

--- 04-projeto-shop-cart/src/index.js
+++ 04-projeto-shop-cart/src/index.js
@@ -2,6 +2,7 @@ import createItem from "./services/item.js";
 import * as cartService from "./services/cart.js";
 
 const myCart = [];
+const myWishList = [];
 
 console.log("Welcome to your Shop Cart!");
 
@@ -9,7 +10,7 @@ const item1 = await createItem("hotwheels ferrari", 20.99, 1);
 const item2 = await createItem("hotwheels lamborghini", 39.99, 3);
 
 await cartService.addItem(myCart, item1);
-await cartService.addItem(myCart, item2);
+await cartService.addItem(myWishList, item2); //reaproveitamento dos modulos para nova feature
 
 console.log("Shop Cart Total is:");
 console.log( await cartService.calculateTotal(myCart)); 
