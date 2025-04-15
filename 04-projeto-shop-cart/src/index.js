b24e87c57438031fc79a560d90f97720861f981c
Myllene Mendes <myllenefms@hotmail.com>
2025-04-15 11:59:30 -0300

    métodos de exibição padronizadas

--- 04-projeto-shop-cart/src/index.js
+++ 04-projeto-shop-cart/src/index.js
@@ -9,8 +9,14 @@ console.log("Welcome to your Shop Cart!");
 const item1 = await createItem("hotwheels ferrari", 20.99, 1);
 const item2 = await createItem("hotwheels lamborghini", 39.99, 3);
 
+//adicionando itens no carrinho
 await cartService.addItem(myCart, item1);
 await cartService.addItem(myWishList, item2); //reaproveitamento dos modulos para nova feature
+await cartService.addItem(myCart, item2);
 
-console.log("Shop Cart Total is:");
-console.log( await cartService.calculateTotal(myCart)); 
+//deletando item no carrinho
+//await cartService.deleteItem(myCart, item2.name);
+//await cartService.deleteItem(myCart, item1.name);
+
+await cartService.displayCart(myCart);
+await cartService.calculateTotal(myCart); 
--- 04-projeto-shop-cart/src/services/cart.js
+++ 04-projeto-shop-cart/src/services/cart.js
@@ -7,8 +7,12 @@ async function addItem(userCart, item) {
 }
 
 //deletar item - excluir o item todo
-async function deleteItem(userCart, item) {
-    
+async function deleteItem(userCart, name) {
+    const index = userCart.findIndex((item) => item.name === name);
+
+    if (index !== -1){
+        userCart.splice(index, 1);
+    }
 }
 
 //remover um item - diminui a quantidade do item
@@ -17,13 +21,22 @@ async function removeItem(userCart, index) {
 }
 
 //calcular o total
-async function calculateTotal(userCart) {
-    return userCart.reduce((total, item) => total + item.subtotal(), 0);
+async function calculateTotal(userCart) {;
+    const result = userCart.reduce((total, item) => total + item.subtotal(), 0)
+    console.log(`\n 💵 Shop Cart Total is: R$ ${result} 💵`);
+}
+
+async function displayCart(userCart) {
+    console.log("\n 🛒 Shop Cart List 🛒");
+    userCart.forEach((item, index) => {
+        console.log(`${index+1}. ${item.name} - R$ ${item.price} | ${item.quantity}x | Subtotal ${item.subtotal()}`)
+    });
 }
 
 export {
     addItem,
     calculateTotal,
     deleteItem,
-    removeItem
+    removeItem,
+    displayCart
 }
